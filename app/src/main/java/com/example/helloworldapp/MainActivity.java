1be7e6fa3b10336dffb3abc8d8121162f763725c
Vbsteam <vbsteam07@gmail.com>
2020-12-06 13:22:06 +0530

    Hello

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.helloworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    TextView textView1, textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        button1= findViewById(R.id.button1);
+        textView1= findViewById(R.id.textView1);
+        textView2= findViewById(R.id.textView2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a= new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(a);
+            }
+        });
+    }
+}
